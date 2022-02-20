db39182611537bdf97b2767984dc8403fbccd1d9
David Rosca <nowrep@gmail.com>
2022-02-20 20:44:58 +0100

    wlcursor: Update to latest ext-screencopy-v1

--- src/wlcursor.c
+++ src/wlcursor.c
@@ -111,24 +111,19 @@ static struct wl_buffer *create_shm_buffer(struct wl_shm *shm,
     return buffer;
 }
 
-static void surface_handle_buffer_info(void *data_,
-    struct zext_screencopy_surface_v1 *surface,
-    enum zext_screencopy_surface_v1_buffer_type type,
-    uint32_t format, uint32_t width, uint32_t height,
-    uint32_t stride)
+static void surface_handle_buffer_info(void *data,
+        struct zext_screencopy_surface_v1 *surface, uint32_t type,
+        uint32_t fourcc, uint32_t width, uint32_t height,
+        uint32_t stride)
+{
+}
+
+static void surface_handle_cursor_buffer_info(void *data_,
+        struct zext_screencopy_surface_v1 *surface, const char *name,
+        uint32_t type, uint32_t format, uint32_t width, uint32_t height,
+        uint32_t stride)
 {
     struct output_data *data = data_;
-
-    if (type == ZEXT_SCREENCOPY_SURFACE_V1_BUFFER_TYPE_NONE) {
-        if (!data->buffer) {
-            blog(LOG_ERROR, "no available shm buffers");
-            return;
-        }
-        zext_screencopy_surface_v1_attach_buffer(surface, data->buffer);
-        zext_screencopy_surface_v1_commit(surface,
-                ZEXT_SCREENCOPY_SURFACE_V1_OPTIONS_WAIT_FOR_DAMAGE);
-        return;
-    }
 
     if (type != ZEXT_SCREENCOPY_SURFACE_V1_BUFFER_TYPE_WL_SHM) {
         return;
@@ -151,53 +146,35 @@ static void surface_handle_buffer_info(void *data_,
     }
 }
 
-static void surface_handle_transform(void *data,
-    struct zext_screencopy_surface_v1 *surface,
-    int32_t transform)
-{
-}
-
-static void surface_handle_ready(void *data_,
-    struct zext_screencopy_surface_v1 *surface)
+static void surface_handle_init_done(void *data_,
+        struct zext_screencopy_surface_v1 *surface)
 {
     struct output_data *data = data_;
 
-    data->have_cursor = true;
-    gs_texture_set_image(data->tex, data->buffer_data,
-            data->buffer_stride, false);
-
-    zext_screencopy_surface_v1_attach_buffer(surface, data->buffer);
-    zext_screencopy_surface_v1_commit(surface,
-            ZEXT_SCREENCOPY_SURFACE_V1_OPTIONS_WAIT_FOR_DAMAGE);
-}
-
-static void capture_output(struct output_data *data);
-
-static void surface_handle_failed(void *data_,
-    struct zext_screencopy_surface_v1 *surface,
-    enum zext_screencopy_surface_v1_failure_reason reason)
-{
-    struct output_data *data = data_;
-
-    if (reason != ZEXT_SCREENCOPY_SURFACE_V1_FAILURE_REASON_CURSOR_MISSING) {
-        blog(LOG_ERROR, "failed to copy surface %d", reason);
+    if (!data->buffer) {
+        blog(LOG_ERROR, "no available shm buffers");
+        return;
     }
 
-    data->have_cursor = false;
-    output_data_reset(data);
-    capture_output(data);
+    zext_screencopy_surface_v1_attach_cursor_buffer(surface,
+            data->buffer, "default");
+    zext_screencopy_surface_v1_damage_cursor_buffer(surface,
+            "default");
+    zext_screencopy_surface_v1_commit(surface,
+            ZEXT_SCREENCOPY_SURFACE_V1_OPTIONS_NONE);
 }
 
 static void surface_handle_damage(void *data,
-    struct zext_screencopy_surface_v1 *surface,
-    uint32_t x, uint32_t y, uint32_t width, uint32_t height)
+        struct zext_screencopy_surface_v1 *surface,
+        uint32_t x, uint32_t y, uint32_t width, uint32_t height)
 {
 }
 
 static void surface_handle_cursor_info(void *data_,
-    struct zext_screencopy_surface_v1 *surface,
-    int32_t pos_x, int32_t pos_y,
-    int32_t hotspot_x, int32_t hotspot_y)
+        struct zext_screencopy_surface_v1 *surface,
+        const char *name, int damaged, int32_t pos_x, int32_t pos_y,
+        int32_t width, int32_t height, int32_t hotspot_x,
+        int32_t hotspot_y)
 {
     struct output_data *data = data_;
 
@@ -207,17 +184,60 @@ static void surface_handle_cursor_info(void *data_,
     data->hotspot_y = hotspot_y;
 }
 
-static void surface_handle_presentation_time(void *data,
-    struct zext_screencopy_surface_v1 *surface,
-    uint32_t sec_hi, uint32_t sec_lo, uint32_t nsec)
+static void surface_handle_cursor_enter(void *data_,
+        struct zext_screencopy_surface_v1 *surface,
+        const char *name)
+{
+    struct output_data *data = data_;
+    data->have_cursor = true;
+}
+
+static void surface_handle_cursor_leave(void *data_,
+        struct zext_screencopy_surface_v1 *surface,
+        const char *name)
+{
+    struct output_data *data = data_;
+    data->have_cursor = false;
+}
+
+static void surface_handle_transform(void *data,
+        struct zext_screencopy_surface_v1 *frame, int32_t transform)
 {
 }
 
-static const struct zext_screencopy_surface_v1_listener frame_listener = {
+static void surface_handle_commit_time(void *data,
+        struct zext_screencopy_surface_v1 *frame, uint32_t tv_sec_hi,
+        uint32_t tv_sec_lo, uint32_t tv_nsec)
+{
+}
+
+static void surface_handle_ready(void *data_,
+    struct zext_screencopy_surface_v1 *surface)
+{
+    struct output_data *data = data_;
+
+    gs_texture_set_image(data->tex, data->buffer_data,
+            data->buffer_stride, false);
+
+    surface_handle_init_done(data, surface);
+}
+
+static void surface_handle_failed(void *data,
+    struct zext_screencopy_surface_v1 *surface,
+    enum zext_screencopy_surface_v1_failure_reason reason)
+{
+    blog(LOG_ERROR, "failed to copy surface %d", reason);
+}
+
+static const struct zext_screencopy_surface_v1_listener surface_listener = {
     .buffer_info = surface_handle_buffer_info,
+    .cursor_buffer_info = surface_handle_cursor_buffer_info,
+    .init_done = surface_handle_init_done,
     .damage = surface_handle_damage,
     .cursor_info = surface_handle_cursor_info,
-    .presentation_time = surface_handle_presentation_time,
+    .cursor_enter = surface_handle_cursor_enter,
+    .cursor_leave = surface_handle_cursor_leave,
+    .commit_time = surface_handle_commit_time,
     .transform = surface_handle_transform,
     .ready = surface_handle_ready,
     .failed = surface_handle_failed,
@@ -226,8 +246,9 @@ static const struct zext_screencopy_surface_v1_listener frame_listener = {
 static void capture_output(struct output_data *data)
 {
     data->surface =
-        zext_screencopy_manager_v1_capture_output_cursor(data->ctx->screencopy, data->output);
-    zext_screencopy_surface_v1_add_listener(data->surface, &frame_listener, data);
+        zext_screencopy_manager_v1_capture_output(data->ctx->screencopy, data->output,
+                ZEXT_SCREENCOPY_MANAGER_V1_OPTIONS_NONE);
+    zext_screencopy_surface_v1_add_listener(data->surface, &surface_listener, data);
 }
 
 static void handle_global(void *data, struct wl_registry *registry,
