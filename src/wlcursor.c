7eddf08aeed4aac6603dd8a6451e6660734b2e9b
David Rosca <nowrep@gmail.com>
2022-10-17 11:44:55 +0200

    wlcursor: Update to latest ext-screencopy-v1

--- src/wlcursor.c
+++ src/wlcursor.c
@@ -33,7 +33,7 @@ struct output_data {
     uint32_t buffer_height;
     uint32_t buffer_stride;
     void *buffer_data;
-    struct ext_screencopy_surface_v1 *surface;
+    struct ext_screencopy_session_v1 *session;
     int32_t pos_x;
     int32_t pos_y;
     int32_t hotspot_x;
@@ -55,9 +55,9 @@ static void output_data_reset(struct output_data *data)
         munmap(data->buffer_data, data->buffer_stride * data->buffer_height);
         data->buffer_data = NULL;
     }
-    if (data->surface) {
-        ext_screencopy_surface_v1_destroy(data->surface);
-        data->surface = NULL;
+    if (data->session) {
+        ext_screencopy_session_v1_destroy(data->session);
+        data->session = NULL;
     }
     if (data->tex) {
         gs_texture_destroy(data->tex);
@@ -116,22 +116,22 @@ static struct wl_buffer *create_shm_buffer(struct wl_shm *shm,
     return buffer;
 }
 
-static void surface_handle_buffer_info(void *data,
-        struct ext_screencopy_surface_v1 *surface, uint32_t type,
+static void session_handle_buffer_info(void *data,
+        struct ext_screencopy_session_v1 *session, uint32_t type,
         uint32_t fourcc, uint32_t width, uint32_t height,
         uint32_t stride)
 {
 }
 
-static void surface_handle_cursor_buffer_info(void *data_,
-        struct ext_screencopy_surface_v1 *surface, const char *name,
-        enum ext_screencopy_surface_v1_input_type input_type,
+static void session_handle_cursor_buffer_info(void *data_,
+        struct ext_screencopy_session_v1 *session, const char *name,
+        enum ext_screencopy_session_v1_input_type input_type,
         uint32_t type, uint32_t format, uint32_t width, uint32_t height,
         uint32_t stride)
 {
     struct output_data *data = data_;
 
-    if (type != EXT_SCREENCOPY_SURFACE_V1_BUFFER_TYPE_WL_SHM) {
+    if (type != EXT_SCREENCOPY_SESSION_V1_BUFFER_TYPE_WL_SHM) {
         return;
     }
 
@@ -152,8 +152,8 @@ static void surface_handle_cursor_buffer_info(void *data_,
     }
 }
 
-static void surface_handle_init_done(void *data_,
-        struct ext_screencopy_surface_v1 *surface)
+static void session_handle_init_done(void *data_,
+        struct ext_screencopy_session_v1 *session)
 {
     struct output_data *data = data_;
 
@@ -162,24 +162,24 @@ static void surface_handle_init_done(void *data_,
         return;
     }
 
-    ext_screencopy_surface_v1_attach_cursor_buffer(surface,
-            data->buffer, "default",
-            EXT_SCREENCOPY_SURFACE_V1_INPUT_TYPE_POINTER);
-    ext_screencopy_surface_v1_damage_cursor_buffer(surface,
-            "default", EXT_SCREENCOPY_SURFACE_V1_INPUT_TYPE_POINTER);
-    ext_screencopy_surface_v1_commit(surface,
-            EXT_SCREENCOPY_SURFACE_V1_OPTIONS_ON_DAMAGE);
+    ext_screencopy_session_v1_attach_cursor_buffer(session,
+            data->buffer, NULL,
+            EXT_SCREENCOPY_SESSION_V1_INPUT_TYPE_POINTER);
+    ext_screencopy_session_v1_damage_cursor_buffer(session,
+            NULL, EXT_SCREENCOPY_SESSION_V1_INPUT_TYPE_POINTER);
+    ext_screencopy_session_v1_commit(session,
+            EXT_SCREENCOPY_SESSION_V1_OPTIONS_ON_DAMAGE);
 }
 
-static void surface_handle_damage(void *data,
-        struct ext_screencopy_surface_v1 *surface,
+static void session_handle_damage(void *data,
+        struct ext_screencopy_session_v1 *session,
         uint32_t x, uint32_t y, uint32_t width, uint32_t height)
 {
 }
 
-static void surface_handle_cursor_info(void *data_,
-        struct ext_screencopy_surface_v1 *surface, const char *name,
-        enum ext_screencopy_surface_v1_input_type input_type,
+static void session_handle_cursor_info(void *data_,
+        struct ext_screencopy_session_v1 *session, const char *name,
+        enum ext_screencopy_session_v1_input_type input_type,
         int damaged, int32_t pos_x, int32_t pos_y,
         int32_t width, int32_t height, int32_t hotspot_x,
         int32_t hotspot_y)
@@ -193,35 +193,35 @@ static void surface_handle_cursor_info(void *data_,
     data->damaged = damaged;
 }
 
-static void surface_handle_cursor_enter(void *data_,
-        struct ext_screencopy_surface_v1 *surface,
+static void session_handle_cursor_enter(void *data_,
+        struct ext_screencopy_session_v1 *session,
         const char *seat_name, uint32_t input_type)
 {
     struct output_data *data = data_;
     data->have_cursor = true;
 }
 
-static void surface_handle_cursor_leave(void *data_,
-        struct ext_screencopy_surface_v1 *surface,
+static void session_handle_cursor_leave(void *data_,
+        struct ext_screencopy_session_v1 *session,
         const char *seat_name, uint32_t input_type)
 {
     struct output_data *data = data_;
     data->have_cursor = false;
 }
 
-static void surface_handle_transform(void *data,
-        struct ext_screencopy_surface_v1 *frame, int32_t transform)
+static void session_handle_transform(void *data,
+        struct ext_screencopy_session_v1 *session, int32_t transform)
 {
 }
 
-static void surface_handle_commit_time(void *data,
-        struct ext_screencopy_surface_v1 *frame, uint32_t tv_sec_hi,
+static void session_handle_presentation_time(void *data,
+        struct ext_screencopy_session_v1 *session, uint32_t tv_sec_hi,
         uint32_t tv_sec_lo, uint32_t tv_nsec)
 {
 }
 
-static void surface_handle_ready(void *data_,
-    struct ext_screencopy_surface_v1 *surface)
+static void session_handle_ready(void *data_,
+    struct ext_screencopy_session_v1 *session)
 {
     struct output_data *data = data_;
 
@@ -230,51 +230,53 @@ static void surface_handle_ready(void *data_,
                 data->buffer_stride, false);
     }
 
-    ext_screencopy_surface_v1_attach_cursor_buffer(surface,
-            data->buffer, "default",
-            EXT_SCREENCOPY_SURFACE_V1_INPUT_TYPE_POINTER);
-    ext_screencopy_surface_v1_commit(surface,
-            EXT_SCREENCOPY_SURFACE_V1_OPTIONS_ON_DAMAGE);
+    ext_screencopy_session_v1_attach_cursor_buffer(session,
+            data->buffer, NULL,
+            EXT_SCREENCOPY_SESSION_V1_INPUT_TYPE_POINTER);
+    ext_screencopy_session_v1_damage_cursor_buffer(session,
+            NULL, EXT_SCREENCOPY_SESSION_V1_INPUT_TYPE_POINTER);
+    ext_screencopy_session_v1_commit(session,
+            EXT_SCREENCOPY_SESSION_V1_OPTIONS_ON_DAMAGE);
 }
 
-static void surface_handle_failed(void *data_,
-    struct ext_screencopy_surface_v1 *surface,
-    enum ext_screencopy_surface_v1_failure_reason reason)
+static void session_handle_failed(void *data_,
+    struct ext_screencopy_session_v1 *session,
+    enum ext_screencopy_session_v1_failure_reason reason)
 {
     struct output_data *data = data_;
     output_data_reset(data);
 
-    if (reason == EXT_SCREENCOPY_SURFACE_V1_FAILURE_REASON_OUTPUT_DISABLED) {
+    if (reason == EXT_SCREENCOPY_SESSION_V1_FAILURE_REASON_OUTPUT_DISABLED) {
         capture_output(data);
         return;
     }
 
-    blog(LOG_ERROR, "failed to copy surface %d", reason);
+    blog(LOG_ERROR, "failed to copy session %d", reason);
 }
 
-static const struct ext_screencopy_surface_v1_listener surface_listener = {
-    .buffer_info = surface_handle_buffer_info,
-    .cursor_buffer_info = surface_handle_cursor_buffer_info,
-    .init_done = surface_handle_init_done,
-    .damage = surface_handle_damage,
-    .cursor_info = surface_handle_cursor_info,
-    .cursor_enter = surface_handle_cursor_enter,
-    .cursor_leave = surface_handle_cursor_leave,
-    .commit_time = surface_handle_commit_time,
-    .transform = surface_handle_transform,
-    .ready = surface_handle_ready,
-    .failed = surface_handle_failed,
+static const struct ext_screencopy_session_v1_listener session_listener = {
+    .buffer_info = session_handle_buffer_info,
+    .cursor_buffer_info = session_handle_cursor_buffer_info,
+    .init_done = session_handle_init_done,
+    .damage = session_handle_damage,
+    .cursor_info = session_handle_cursor_info,
+    .cursor_enter = session_handle_cursor_enter,
+    .cursor_leave = session_handle_cursor_leave,
+    .presentation_time = session_handle_presentation_time,
+    .transform = session_handle_transform,
+    .ready = session_handle_ready,
+    .failed = session_handle_failed,
 };
 
 static void capture_output(struct output_data *data)
 {
-    if (data->surface || !data->ctx->screencopy) {
+    if (data->session || !data->ctx->screencopy) {
         return;
     }
 
-    data->surface =
+    data->session =
         ext_screencopy_manager_v1_capture_output(data->ctx->screencopy, data->output, 0);
-    ext_screencopy_surface_v1_add_listener(data->surface, &surface_listener, data);
+    ext_screencopy_session_v1_add_listener(data->session, &session_listener, data);
 }
 
 static void handle_global(void *data, struct wl_registry *registry,
